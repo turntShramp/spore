b83f045b0cdf8eadcd5b621f98282c46d746eff5
shramp <seamusmurchadh@gmail.com>
2019-03-26 00:10:23 -0400

    initial commit

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: developement_USERNAME,
+    password: developement_PASSWORD,
+    database: developement_DATABASE,
+    host: developement_HOST,
+    dialect: developement_DIALECT
+  },
+  test: {
+    username: test_USERNAME,
+    password: test_PASSWORD,
+    database: test_DATABASE,
+    host: test_HOST,
+    dialect: test_DIALECT
+  },
+  production: {
+    username: process.env.production_USERNAME,
+    password: process.env.production_PASSWORD,
+    database: process.env.production_DATABASE,
+    host: process.env.production_HOST,
+    dialect: production_DIALECT
+  }
+};
