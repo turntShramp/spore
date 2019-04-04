a1a345e7b8a0daa8cd9aa47bfffc38d92ddd7d6a
tommaroney <jasonbeisiegel@gmail.com>
2019-04-03 22:18:56 -0400

    updated config.js to support AWS deploy

--- config/config.js
+++ config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.prod_USERNAME,
+    "password": process.env.prod_PASSWORD,
+    "database": process.env.prod_DATABASE,
+    "host": process.env.prod_HOST,
     "dialect": "mysql"
   }
 }
