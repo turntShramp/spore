57e0c149da58c8ff08e3a073cf6c0b3d176d55e8
tommaroney <jasonbeisiegel@gmail.com>
2019-04-04 10:05:17 -0400

    building scalable session storage

--- config/config.js
+++ config/config.js
@@ -4,20 +4,23 @@ module.exports = {
     "password": process.env.dev_PASSWORD,
     "database": process.env.dev_DATABASE,
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "storage": "./session.mysql"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "storage": "./session.mysql"
   },
   "production": {
     "username": process.env.prod_USERNAME,
     "password": process.env.prod_PASSWORD,
     "database": process.env.prod_DATABASE,
     "host": process.env.prod_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "storage": "./session.mysql"
   }
 }
