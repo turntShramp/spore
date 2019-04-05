00d3d86590e3ee53569e2657288caf73cae919b3
Mike B <michael.byron@gmail.com>
2019-04-05 16:38:13 -0400

    Auth working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,15 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app, passport) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", isLoggedIn, function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/admin", function(req, res) {
+  app.post("/api/admin", isLoggedIn, function (req, res) {
     const postMushroom = req.body;
     let newMushroom = {
       latinName: postMushroom.latinName,
@@ -19,16 +19,51 @@ module.exports = function(app) {
       mushroom_photo: postMushroom.mushroom_photo,
       thumbnail_photo: postMushroom.thumbnail_photo
     }
-    db.Mushroom.create(newMushroom).then(function(mushroom) {
+    db.Mushroom.create(newMushroom).then(function (mushroom) {
       mushroom.addAllIcon
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", isLoggedIn, function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
+
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/user',
+
+    failureRedirect: '/login'
+  }
+
+  ));
+
+  app.post('/signin', passport.authenticate('local-signin', {
+    successRedirect: '/user',
+
+    failureRedirect: '/login'
+}
+
+));
+
+  app.get("/logout", function (req, res) {
+    req.session.destroy(function (err) {
+
+      res.redirect('/');
+
+    });
+  })
+
+  function isLoggedIn(req, res, next) {
+
+    if (req.isAuthenticated())
+
+      return next();
+
+    res.redirect('/login');
+
+  }
+
 };
